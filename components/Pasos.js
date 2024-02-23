c9f9151289fff2a4acc6e6303def4dc71f62bd44
leandro-santiago <69202675+leandros1793@users.noreply.github.com>
2024-02-23 15:47:51 -0300

    pruebas2

--- components/Pasos.js
+++ components/Pasos.js
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 const pasos = [
   { paso: 1, nombre: "Menu", url: "/" },
   { paso: 2, nombre: "Resumen", url: "/resumen" },
-  { paso: 3, nombre: "Datos y Total", url: "/total" },
+  { paso: 3, nombre: "Total", url: "/total" },
 ];
 
 const Pasos = () => {
